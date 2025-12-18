94162845ed28646811317288cd2ab66c3ef24883
BRIGHTDIC01 <thankgod.bright@binghamuni.edu.ng>
2025-12-18 19:07:42 +0100

    Add files via upload

---
+++ IGALA_DICT.py
@@ -0,0 +1,69 @@
+print("hello user, below are a list of words in igala language that can be translated to english, select which word you wish to translate to english")
+print("Please, select an igala word from the list; awa, agba, ona, una, omi, ikpolo, eju_ona, eja, ujewn, otakada, shekpulu, eda, ejo, usha, ikpa, akpe, abia, obala, ugba, obe, oche")
+word= input("Enter the igala word to translate: ")
+igala_dict ={
+    "awa":'weldone',
+    'agba':'ThankYou',
+    "ona":"tomorrow",
+    "una":"fire",
+    "omi":"water",
+    "ikpolo":"stone",
+    "eju_ona":"road",
+    "eja":"fish",
+    "ujewn":"food",
+    "otakada":"book",
+    "shekpulu":"school",
+    "eda":"shoe",
+    "ejo":"snake",
+    "usha":"pot",
+    "ikpa":"bag",
+    "akpe":"scorpion",
+    "abia":"dog",
+    "obala":"cat",
+    "ugba":"plate",
+    "obe":"knive",
+    "oche":"soap"
+
+}
+if  word== ("awa"):
+    print("awa translates:", igala_dict["awa"], "in english")
+if word== ("agba"):
+    print("agba translates:", igala_dict["agba"], "in english")
+if word== ("ona"):
+    print("ona translates:", igala_dict["ona"], "in english")
+if word== ("una"):
+    print("una translates:", igala_dict["una"], "in english")
+if word== ("omi"):
+    print("omi translates:", igala_dict["omi"], "in english")
+if word== ("ikpolo"):
+    print("ikpolo translates:", igala_dict["ikpolo"], "in english")
+if word== ("eju_ona"):
+    print("eju_ona translates:", igala_dict["eju_ona"], "in english")
+if word== ("eja"):
+    print("eja translates:", igala_dict["eja"], "in english")
+if word== ("ujewn"):
+    print("ujewn translates:", igala_dict["ujewn"], "in english")
+if word== ("otakada"):
+    print("otakada translates:", igala_dict["otakada"], "in english")
+if word== ("shekpulu"):
+    print("shekpulu translates:", igala_dict["shekpulu"], "in english")
+if word== ("eda"):
+    print("eda translates:", igala_dict["eda"], "in english")
+if word== ("ejo"):
+    print("ejo translates:", igala_dict["ejo"], "in english")
+if word== ("usha"):
+    print("usha translates:", igala_dict["usha"], "in english")
+if word== ("ikpa"):
+    print("ikpa translates:", igala_dict["ikpa"], "in english")
+if word== ("akpe"):
+    print("akpe translates:", igala_dict["akpe"], "in english")
+if word== ("abia"):
+    print("abia translates:", igala_dict["abia"], "in english")
+if word== ("obala"):
+    print("obala translates:", igala_dict["obala"], "in english")
+if word== ("ugba"):
+    print("ugba translates:", igala_dict["ugba"], "in english")
+if word== ("obe"):
+    print("obe translates:", igala_dict["obe"], "in english")
+if word== ("oche"):
+    print("oche translates:", igala_dict["oche"], "in english")

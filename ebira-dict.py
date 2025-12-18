c9bab9360b455bc1fd7b2c621ffb8d3c11fa7c04
Joel-momoh <momohadeiza92@gmail.com>
2025-12-18 13:07:52 +0100

    Add files via upload

---
+++ ebira-dict.py
@@ -0,0 +1,30 @@
+
+Ebira_dict ={
+    "nyene" : "good morning",
+    "anyar' oodu" : "good afternoon",
+    "ngwao " : "hello",
+    "nyasse " : "welcome",
+    "otura geri " : "car",
+    "na" : "go",
+    "be" : "come",
+    "oose" : "walk ",
+    "avo" : "thanks",
+    "ha" : "wake",
+    "su' ara" : "sleep",
+    "eyi" : "hair",
+    "iresi" : "head",
+    "ize" : "wealth",
+    "engworo" : "peace",
+    "ohu" : "market",
+    " " : " ",
+    " " : " ",
+    " " : " ",
+    " " : " ",
+}
+#Ask  user for input
+word= input("Enter an Ebira word:").lower()
+#chech for word
+if word in Ebira_dict:
+    print(f"The English meaning of '{word}' is: {Ebira_dict[word]}")
+else:
+    print("Sorry, that word is not in the dictionary.")

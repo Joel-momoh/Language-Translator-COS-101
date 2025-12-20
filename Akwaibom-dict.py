fd78ce18fa09feff8fcae7897f07dba2a2bbe43b
sesughjames4-hub <sesughjames4@gmail.com>
2025-12-20 16:45:17 +0100

    Update Akwaibom-dict.py

--- Akwaibom-dict.py
+++ Akwaibom-dict.py
@@ -1,1 +1,34 @@
-print("hello user, below are the list of words that can be translated from akwaibom to english      
+print("hello user, below are the list of words that can be translated from akwaibom to english, select which word you wish to translate to english below")
+print("please, select an akwaibom word you wish to translate from the list below; afon, ndidia, mmong, mbonomi, ufokngwet, eyen, ufok, abasi, ubong, ete, eka, uduak, ekpe, usan, ekpan, usan, ekpaan, afu, tie, di, sosong")
+word_input("Enter the akwaibom word you want to translate to english:")
+akwaibom_dict ={
+  'afon': 'good',
+  'ndidia': 'food',
+  'mmong': 'water',
+  'mbonomi': 'family',
+  'ufokgwet': 'school',
+  'eyen': 'baby',
+  'ufok': 'house',
+  'abasi': 'god',
+  'ubong': 'king',
+  'ete': 'father',
+  'eka': 'mother',
+  'uduak': 'time',
+  'ekpe': 'lion',
+  'usan': 'plate',
+  'ekpaan': 'spoon',
+  'afu': 'you',
+  'tie': 'sitdown',
+  'di': 'come',
+  'sosong': 'thankyou',
+  'daka': 'go'
+  
+}
+if word== ("afon"):
+  print("afon translate:", akwaibom_dict["afon"], "english")
+if word== ("ndidia"):
+  print("ndidia translate:", akwaibom_dict["ndidia"], "english")
+if word== ("mmong"):
+  print("mmong translate:", akwaibom_dict["mmong"], "english")
+if word== ("mbonomi"):
+  print("mbonomi translate:", akwaibom_dict["mmong"], "english")
